97a7c049bcc3e8a510c7dc60ecee267edc44f078
Audric <75308203+Audric-P@users.noreply.github.com>
2020-12-20 09:45:36 +0100

    Modèles supprimés
    Les modèles qui servaient de maquettes ne sont plus nécessaire.

--- script.js
+++ script.js
@@ -1,72 +1,3 @@
-function calendarMonth() {
-
-    var monthNames = ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jui', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var jours = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'];
-    var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    var today = new Date();
-    var thisDay = today.getDate();
-    var year = today.getYear();
-    year <= 200 ? year += 1900 : null;
-
-    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-        monthDays[1] = 29;
-    }
-    var nDays = monthDays[today.getMonth()];
-    var firstDay = today;
-    firstDay.setDate(0);
-
-    firstDay.getDate() == 2 ? firstDay.setDate(0) : null;
-
-    var startDay = firstDay.getDay();
-
-    var table = document.createElement('table');
-
-    var tbRow = table.insertRow(-1);
-
-    var tbHead = document.createElement("th");
-    tbHead.setAttribute('colspan', '7');
-    var tbhtxt = document.createTextNode(monthNames[today.getMonth() + 1] + '.' + year);
-    tbHead.appendChild(tbhtxt);
-
-    tbRow.appendChild(tbHead);
-
-    var tbRow = table.insertRow(-1);
-
-    for (var i = 0; i < jours.length; i++) {
-
-        tbRow.insertCell(-1).appendChild(document.createTextNode(jours[i]));
-    }
-
-    var tbRow = document.createElement("tr");
-
-    var column = 0;
-
-    for (var i = 0; i < startDay; i++) {
-        tbRow.insertCell(0);
-        column++;
-    }
-
-    for (var i = 1; i <= nDays; i++) {
-
-        var tdd = tbRow.insertCell(-1);
-
-        i == thisDay ? tdd.style.color = "#FF0000" : null;
-
-        tdd.appendChild(document.createTextNode(i));
-
-        column++;
-        if (column == 7) {
-            table.appendChild(tbRow);
-            var tbRow = document.createElement("tr");
-            column = 0;
-        }
-
-        i == nDays ? table.appendChild(tbRow) : null;
-
-    }
-    document.getElementById('contcalendar').appendChild(table);
-}
-
 function calendarWeek() {
     // Noms des mois
     var monthNames = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
@@ -145,36 +76,7 @@ function calendarWeek() {
         tbData.appendChild(txtarea);
 
     }
-    
-
-
-    // for (var i = 1; i <= nDays; i++) {
-
-    //     // Nouvelle case
-    //     var tdd = tbRow.insertCell(-1);
-
-    //     // Couleur rouge pour aujourd'hui
-    //     i == thisDay ? tdd.style.color = "#FF0000" : null;
-
-    //     // On met dans la cases le texte
-    //     tdd.appendChild(document.createTextNode(i));
-
-    //     // Compteur de colonne
-    //     column++;
-    //     // Si on a fait 7 colonnes alors :
-    //     if (column == 7) {
-    //         // Nouvelle ligne
-    //         table.appendChild(tbRow);
-    //         var tbRow = document.createElement("tr");
-    //         column = 0;
-    //     }
-
-    //     i == nDays ? table.appendChild(tbRow) : null;
-
-    // }
-
-    document.getElementById('secondcalendar').appendChild(table);
+    document.getElementById('calendar').appendChild(table);
 }
 
-typeof window.addEventListener == 'undefined' ? window.attachEvent("onload", calendarMonth) : addEventListener('load', calendarMonth, false);
 typeof window.addEventListener == 'undefined' ? window.attachEvent("onload", calendarWeek) : addEventListener('load', calendarWeek, false);
